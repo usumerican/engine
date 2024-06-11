f614e39ad749c864b32fb948986af1947bf23d4a
usumerican <95164144+usumerican@users.noreply.github.com>
2024-06-11 17:31:05 +0900

    v0.9.1

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+set -euxo pipefail
 
 npm run zig:test
 npm run zig:wasm
 npm run build
 npm run lib
 
-zig build -Dtarget=x86_64-windows -Dcpu=x86_64 -Dstrip=true -Dname=engine-windows-x86_64 --prefix dist --verbose --summary all --release=fast
-zig build -Dtarget=x86_64-windows -Dcpu=x86_64_v2 -Dstrip=true -Dname=engine-windows-x86_64_v2 --prefix dist --verbose --summary all --release=fast
-zig build -Dtarget=x86_64-windows -Dcpu=x86_64_v3 -Dstrip=true -Dname=engine-windows-x86_64_v3 --prefix dist --verbose --summary all --release=fast
-zig build -Dtarget=x86_64-windows -Dcpu=x86_64_v4 -Dstrip=true -Dname=engine-windows-x86_64_v4 --prefix dist --verbose --summary all --release=fast
+zig build -Dtarget=x86_64-windows -Dcpu=x86_64 -Dname=zshogi-windows-x86_64 -Dstrip=true --release=fast --prefix dist --verbose --summary all
+zig build -Dtarget=x86_64-windows -Dcpu=x86_64_v2 -Dname=zshogi-windows-x86_64_v2 -Dstrip=true --release=fast --prefix dist --verbose --summary all
+zig build -Dtarget=x86_64-windows -Dcpu=x86_64_v3 -Dname=zshogi-windows-x86_64_v3 -Dstrip=true --release=fast --prefix dist --verbose --summary all
+zig build -Dtarget=x86_64-windows -Dcpu=x86_64_v4 -Dname=zshogi-windows-x86_64_v4 -Dstrip=true --release=fast --prefix dist --verbose --summary all
+
+tree -s dist
